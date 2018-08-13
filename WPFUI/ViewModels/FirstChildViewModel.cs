32212099e61ac10ff537e1c8f15b40ff5ba8f1b8
Witek <Witek@DESKTOP-L5KJMIO>
2018-08-13 21:47:14 +0200

    Dwa okna

---
+++ WPFUI/ViewModels/FirstChildViewModel.cs
@@ -0,0 +1,113 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFUI.Models;
+
+namespace WPFUI.ViewModels
+{
+    public class FirstChildViewModel : Screen
+    {
+        public FirstChildViewModel()
+        {
+            People.Add(new PersonModel { FirstName = "Tim", LastName = "Corey" });
+            People.Add(new PersonModel { FirstName = "John", LastName = "Cartney" });
+            People.Add(new PersonModel { FirstName = "Sue", LastName = "Storm" });
+        }
+
+
+
+        private string _firstName = "Tim";
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        private string _lastName = "Scout";
+
+        public string LasttName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LasttName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LasttName} ";
+            }
+
+        }
+
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+
+        public BindableCollection<PersonModel> People
+        {
+            get
+            {
+                return _people;
+            }
+            set
+            {
+                _people = value;
+                NotifyOfPropertyChange(() => People);
+            }
+        }
+
+        private PersonModel _selectedPerson;
+
+        public PersonModel SelectedPerson
+        {
+            get
+            {
+                return _selectedPerson;
+            }
+            set
+            {
+                _selectedPerson = value;
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+
+        public bool CanClearText(string firstName, string lastName)
+        {
+            //return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
+            if (String.IsNullOrWhiteSpace(firstName) && String.IsNullOrWhiteSpace(lastName))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void ClearText(string firstName, string lastName)
+        {
+            FirstName = "";
+            LasttName = "";
+        }
+
+    }
+}
--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -8,105 +8,19 @@ using WPFUI.Models;
 
 namespace WPFUI.ViewModels
 {
-    public class ShellViewModel : Screen
+    //public class ShellViewModel : Screen   - Dziedziczenie po Screen jest dobre dla jednego okna, dla wielu okien lepsze jest Conductor
+    public class ShellViewModel : Conductor<object>
     {
-        public ShellViewModel()
+       
+
+        public void LoadPageOne()
         {
-            People.Add(new PersonModel {FirstName = "Tim", LastName = "Corey" });
-            People.Add(new PersonModel { FirstName = "John", LastName = "Cartney" });
-            People.Add(new PersonModel { FirstName = "Sue", LastName = "Storm" });
+            ActivateItem(new FirstChildViewModel());
         }
 
-
-
-        private string _firstName = "Tim";
-
-        public string FirstName
+        public void LoadPageTwo()
         {
-            get
-            {
-                return _firstName;
-            }
-            set
-            {
-                _firstName = value;
-                NotifyOfPropertyChange(() => FirstName);
-                NotifyOfPropertyChange(() => FullName);
-            }
-        }
-
-        private string _lastName = "Scout";
-
-        public string LasttName
-        {
-            get
-            {
-                return _lastName;
-            }
-            set
-            {
-                _lastName = value;
-                NotifyOfPropertyChange(() => LasttName);
-                NotifyOfPropertyChange(() => FullName);
-            }
-        }
-
-        public string FullName
-        {
-            get
-            {
-                return $"{FirstName} {LasttName} ";
-            }
-            
-        }
-
-        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
-
-        public BindableCollection<PersonModel> People
-        {
-            get
-            {
-                return _people;
-            }
-            set
-            {
-                _people = value;
-                NotifyOfPropertyChange(() => People);
-            }
-        }
-
-        private PersonModel _selectedPerson;
-
-        public PersonModel SelectedPerson
-        {
-            get
-            {
-                return _selectedPerson;
-            }
-            set
-            {
-                _selectedPerson = value;
-                NotifyOfPropertyChange(() => SelectedPerson);
-            }
-        }
-
-        public bool CanClearText(string firstName, string lastName)
-        {
-            //return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
-            if(String.IsNullOrWhiteSpace(firstName) && String.IsNullOrWhiteSpace(lastName))
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        public void ClearText(string firstName, string lastName)
-        {
-            FirstName = "";
-            LasttName = "";
+            ActivateItem(new SecondChildViewModel());
         }
 
 
